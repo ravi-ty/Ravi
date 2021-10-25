152e0e6c6369cc4268bcacc4e1e5be5952afccb9
ravi-ty <91381775+ravi-ty@users.noreply.github.com>
2021-10-25 12:59:43 +0530

    Add files via upload

---
+++ React_JS/ReduxCounterClass.js
@@ -0,0 +1,39 @@
+import styles from './Counter.module.css';
+import {Component} from "react";
+import {connect} from "react-redux";
+
+class ReduxCounterClass extends Component {
+
+    incrementHandler(){
+        this.props.increment();
+    }
+    decrementHandler(){
+        this.props.decrement();
+    }
+
+    render(){
+        return (
+            <main className={styles.counter}>
+              <h1>Redux Counter</h1>
+              <div className={styles.value}>{this.props.counter}</div>
+              <div>
+                <button onClick={this.incrementHandler.bind(this)}>Increment</button>
+                <button onClick={this.decrementHandler.bind(this)}>Decrement</button>
+              </div>
+            </main>
+          );
+    }
+}
+
+const mapStateToProps = (state) => {
+    return {
+        counter : state.counter
+    }
+}
+const mapDispatchToProps = (dispatch) => {
+    return {
+        increment : dispatch({type:"increment"}),
+        decrement : dispatch({type:"decrement"})
+    }
+}
+export default connect(mapStateToProps,mapDispatchToProps)(ReduxCounterClass);
