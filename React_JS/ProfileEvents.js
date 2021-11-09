c379b58f59359303fad96017adabea6647758531
ravi-ty <91381775+ravi-ty@users.noreply.github.com>
2021-11-09 12:50:00 +0530

    Add files via upload

---
+++ React_JS/ProfileEvents.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ProfileForms from "./ProfileForms";
+import "./ProfileEvents.css";
+
+const ProfileEvents = (props) => {
+    
+    return (
+        <div className="new-expense">
+            <ProfileForms></ProfileForms>
+        </div>
+    )
+
+}
+
+export default ProfileEvents;
