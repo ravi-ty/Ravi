db40158653fa4bab1c7b8261460b1b582893a5f0
ravi-ty <91381775+ravi-ty@users.noreply.github.com>
2021-11-11 12:15:39 +0530

    Add files via upload

---
+++ React_JS/Fragmnts.js
@@ -0,0 +1,12 @@
+import React,{Fragment} from "react";
+
+function Fragmnts(){
+    return (
+        <Fragment>
+            <h1 style={{color:"red",backgroundColor:"white"}}>HELLO</h1>
+            <h2 style={{color:"green",backgroundColor:"white"}}>FRAGMENT EXAMPLE</h2>
+        </Fragment>
+    )
+}
+
+export default Fragmnts;
